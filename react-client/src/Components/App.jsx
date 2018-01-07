deef3bd2451c20c885c4f7dfa76d8b59dae35b52
AJ Pawlicki <aloysiuspaw@gmail.com>
2018-01-07 01:47:30 -0800

    Refactor client and server to maintain separation of concerns

--- react-client/src/Components/App.jsx
+++ react-client/src/Components/App.jsx
@@ -1,37 +1,98 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-import Game from '../../game/connect4.js';
 import GameBoard from './GameBoard.jsx';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.game = new Game();
-    this.game.initBoard();
-
+    this.fetchGame();
+    
     this.state = {
-      board: this.game.board
-    }
+      board: [],
+      player: null,
+      isWinner: false,
+      isTieGame: false,
+      errorRetrievingData: false,
+      isLoadingData: false
+    };
 
     this.handleCellClick = this.handleCellClick.bind(this);
   }
+  
+  fetchGame() {
+    this.setState({
+      isLoadingData: true,
+      errorRetrievingData: false
+    });
+
+    fetch('fetchGame')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        board: data.board,
+        player: data.player,
+        isLoadingData: false,
+        errorRetrievingData: false
+      });
+    })
+    .catch(err => {
+      console.error(err);
+
+      this.setState({
+        errorRetrievingData: true,
+        isLoadingData: false
+      });
+    });
+  }
 
   handleCellClick(col) {
-    const move = this.game.dropPiece(col);
-    if (move) this.game.togglePlayer();
-    if (this.game.checkWinner(move)) console.log('winner!');
+    if (this.state.isWinner || this.state.isTieGame) return;
 
     this.setState({
-      board: this.state.board,
-      lastMove: move
+      isLoadingData: true,
+      errorRetrievingData: false
+    });
+
+    fetch('postMove', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ col: col })
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.isWinner) this.setState({ isWinner: true });
+      if (data.isTieGame) this.setState({ isTieGame: true });
+
+      this.setState({
+        errorRetrievingData: false,
+        isLoadingData: false
+      });
+
+      this.fetchGame();
+    })
+    .catch(err => {
+      console.error(err);
+
+      this.setState({
+        errorRetrievingData: true,
+        isLoadingData: false
+      });
     });
   }
 
   render() {
     return (
       <div>
+        {this.state.isWinner ? <span className="alert">Player {this.player} wins!</span> : null}
+        {this.state.isTieGame ? <span className="alert">Tie game!</span> : null}
+        {this.state.isLoadingData ? <span className="alert">Loading...</span> : null}
+        {this.state.errorRetrievingData ? <span className="alert">There was an error.</span> : null}
+
         <GameBoard board={this.state.board} handleCellClick={this.handleCellClick} />
       </div>
     );
--- server/app.js
+++ server/app.js
@@ -4,11 +4,37 @@ const app = express();
 const Connect4 = require('./game-logic/Connect4');
 
 let game = new Connect4();
+game.initBoard();
 
 app.use(express.static(__dirname + '/../react-client/dist/'));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/fetchGame', (req, res) => {
   res.send(game);
+});
+
+app.post('/postMove', (req, res) => {
+  const { col } = req.body;
+
+  const move = game.dropPiece(col);
+
+  const data = {};
+
+  if (move) {
+    if (game.isWinner) data.isWinner = true;
+    if (game.isTieGame) data.isWinner = true;
+    
+    if (!game.isWinner && !game.isTieGame) game.togglePlayer();
+    
+    res.send(data);
+  } else {
+    data.error = 'Invalid move.';
+    
+    res.send(data);
+  }
+
 });
 
 app.listen(4000, () => console.log('Listening on port 4000!'));
